06374891fc4d147d691fab1cccb356936fda2961
zsoobratty <49350914+zsoobratty@users.noreply.github.com>
2020-07-17 15:57:02 +0100

    destructure data to only what is required

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import React from 'react';
 import Cards from './components/Cards/Cards'
 import Chart from './components/Chart/Chart'
 import CountryPicker from './components/CountryPicker/CountryPicker'
+import { fetchData } from './api'
 import styles from './App.module.css'
 
 class App extends React.Component {
+  state = {
+    data: {},
+  }
+
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+
+    this.setState({ data: fetchedData })
+  }
+
   render() {
+    const { data } = this.state
     return (
       <div className={styles.container}>
-        <Cards />
+        <Cards data={data}/>
         <CountryPicker />
         <Chart />
       </div>
