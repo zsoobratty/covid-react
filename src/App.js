3c63f7c3cad7b94d9a6aa983313591d80f70c359
zsoobratty <49350914+zsoobratty@users.noreply.github.com>
2020-07-17 15:43:58 +0100

    convert App to class based component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import Cards from './components/Cards/Cards'
+import Chart from './components/Chart/Chart'
+import CountryPicker from './components/CountryPicker/CountryPicker'
+import styles from './App.module.css'
 
-function App() {
-  return (
-    <div>
-      Covid Tracker
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className={styles.container}>
+        <Cards />
+        <CountryPicker />
+        <Chart />
+      </div>
+    );
+  }
 }
 
 export default App;
