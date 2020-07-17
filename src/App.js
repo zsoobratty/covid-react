c7ff78ac8431559ebcb657b7c700a5d8ea79dcd0
zsoobratty <49350914+zsoobratty@users.noreply.github.com>
2020-07-17 17:35:47 +0100

    css tweaks

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Chart from './components/Chart/Chart'
 import CountryPicker from './components/CountryPicker/CountryPicker'
 import { fetchData } from './api'
 import styles from './App.module.css'
+import covidImage from './images/covid.png'
 
 class App extends React.Component {
   state = {
@@ -27,6 +28,7 @@ class App extends React.Component {
     const { data, country } = this.state
     return (
       <div className={styles.container}>
+        <img className={styles.image} src={covidImage} alt="COVID-19"/>
         <Cards data={data}/>
         <CountryPicker handleCountryChange={this.handleCountryChange}/>
         <Chart data={data} country={country} />
